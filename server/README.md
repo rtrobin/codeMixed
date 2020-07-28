f14d9b4c8a782f4cdc8a7672648193eeba23a11b
tangyijun <robintang.116@gmail.com>
2020-07-28 18:50:39 +0800

    add a simple pyTorch inference server example

---
+++ server/README.md
@@ -0,0 +1,15 @@
+# A simple pyTorch inference server example
+
+## A simple inference server example with pyTorch and flask+gunicorn
+
+Run the server:
+
+`gunicorn -c gunicorn.py main:app`
+
+Test with 1000 concurrent requests:
+
+`python client.py`
+
+## notes
+
+Modify `gunicorn.py` to change gunicorn settings. With different worker mode, GPU model instances behave differently.
---
+++ server/client.py
@@ -0,0 +1,21 @@
+import requests
+import time
+from gevent.pool import Pool
+
+def req(_):
+    st = time.time()
+    resp = requests.post(
+        "http://localhost:5000/predict",
+        files={"file": open('./dog.jpg','rb')})
+    et = time.time()
+    print(f'req time: {et-st}, {resp.json()}')
+
+def main():
+    req_numbers = [i for i in range(1000)]
+    pool = Pool()
+
+    pool.map(req, req_numbers)
+    return
+
+if __name__ == '__main__':
+    main()
