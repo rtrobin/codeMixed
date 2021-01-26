fc3e4e7c8eb328ee8cea73333b41eb7519d1fcc5
rtrobin <robintang.116@gmail.com>
2021-01-26 10:46:24 +0800

    update jit test code.

--- triton/export.py
+++ triton/export.py
@@ -26,5 +26,22 @@ def export():
     torch.jit.save(script_model, 'resnet152.script.gpu.pth')
     return
 
+def test():
+    # device = torch.device('cpu')
+    device = torch.device('cuda')
+
+    input = torch.rand(size=(128, 3, 512, 512), dtype=torch.float32).to(device)
+
+    script_model = torch.jit.load('resnet152.script.gpu.pth')
+    traced_model = torch.jit.load('resnet152.traced.gpu.pth')
+
+    with torch.no_grad():
+        traced_output = traced_model(input)
+        script_output = script_model(input)
+
+    print(torch.max(script_output-traced_output))
+    return
+
 if __name__ == "__main__":
     export()
+    test()
